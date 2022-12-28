2df12afefd9b92809fee8e738ff38ab9e7271628
murpin <qw04011@gmail.com>
2022-12-28 14:14:35 +0900

    style: Design TodoList wireframe

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,62 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import styled  from 'styled-components';
+import * as api from './api';
+
+
+const Main = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
+  opacity: 0.5;
+`;
+
+const TodoListBox = styled.div`
+  width: 512px;
+  height: 614px;
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+`
+
+const TodoHeader = styled.div`
+  width: 512px;
+  height: 100px;
+  border: 1px solid red;  
+`
+
+const TodoContent = styled.div`
+  width: 512px;
+  height: 514px;
+  border: 1px solid black;
+  overflow: scroll;
+`
+const TodoFooter = styled.div`
+  width: 512px;
+  height: 100px;
+  border: 1px solid red;  
+`
 
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <Main>
+      <TodoListBox>
+        <TodoHeader>
+          오늘 할 일  
+        </TodoHeader>
+        <TodoContent>
+        </TodoContent>
+        <TodoFooter>
+          추가하기
+        </TodoFooter>
+      </TodoListBox>
+    </Main>
   );
 }
 
+
 export default App;
+
