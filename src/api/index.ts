edb8fe0300f12a0c9af80d7680f304bef03370eb
murpin <qw04011@gmail.com>
2022-12-28 17:33:43 +0900

    style: edit api naming

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,4 @@
-export const getTodoList = async () => {
+export const fetchTodoList = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/user/1/todos')
   return res.json();
 }
