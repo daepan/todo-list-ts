8965d4d0ab297f7a1a5b580fe9b453168f54af1e
murpin <qw04011@gmail.com>
2022-12-28 23:45:57 +0900

    feat: add todoList api (delete, add, update)

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,38 @@
+import { CompletedChangeRequest, TodoItemRequest} from './interface';
+
 export const fetchTodoList = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/user/1/todos')
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=5");
   return res.json();
+}
+
+export const deleteTodoListItem = async (Id: number) => {
+  await fetch(`https://jsonplaceholder.typicode.com/todos/${Id}`, {
+    method: 'DELETE',
+  })
+}
+
+export const addTodoListItem = async (todoItem: TodoItemRequest) => {
+  await fetch(`https://jsonplaceholder.typicode.com/todos`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      userId: todoItem.id,
+      title: todoItem.title,
+      completed: todoItem.completed,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+}
+
+export const updateTodoListItem = async (todoItem: CompletedChangeRequest) => {
+  await fetch(`https://jsonplaceholder.typicode.com/todos/${todoItem.id}`, {
+    method: 'PATCH',
+    body: JSON.stringify({
+      completed: todoItem.completed
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
 }
