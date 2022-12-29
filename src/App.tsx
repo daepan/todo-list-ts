404981f7190a54155bf6f2794413cd45b5bec337
murpin <qw04011@gmail.com>
2022-12-29 10:14:01 +0900

    refactor: divide todoContent, useMutation api dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { useMutation, useQueryClient } from 'react-query';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import TodoContent from './components/TodoContent';
 import useTodoList from './hooks/useTodoList';
+import useTodoListMutation from './hooks/useTodoListMutation';
 import styled  from 'styled-components';
-import * as api from './api';
 
 interface TodoListItemElements {
   userId: number
@@ -39,73 +38,6 @@ const TodoHeader = styled.div`
   align-items: center;
   color: rgb(0, 0, 204);
   font-size: 65px;  
-`
-
-const TodoContent = styled.div`
-  width: 512px;
-  height: 514px;
-  overflow: scroll;
-`
-const TodoListItem = styled.div`
-  width: 492px;
-  height: 70px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: 10px 10px;
-  color: #333;
-  background-color: rgba(51,51,255,.5);
-  margin-bottom: 15px;
-  border-radius: 21px;
-  box-shadow: 5px 5px 5px 5px gray;
-  filter: brightness(1.75);
-
-  &.item-enter {
-    opacity: 0;
-  }
-
-  &.item-enter-active {
-    opacity: 0.9;
-    transition: all 500ms;
-  }
-
-  &.item-exit {
-    opacity: 0.01;
-    transition: opacity 1000ms ease-out;
-  }
-`
-const ItemTitle = styled.div`
-  width: 400px;
-  margin-left: 10px;
-`
-
-const ItemCheckBox = styled.input`
-  width: 20px;
-  height: 20px;
-`
-
-const ItemDeleteButton = styled.button`
-  width: 30px;
-  height: 30px;
-  float:right;
-  display:flex;
-  text-align: center;
-  align-items:center;
-  justify-content:center;
-  font-size: 1.5em;
-  background-color: rgb(255, 0, 117);
-  outline: 0;
-  border-radius: 5px;
-  border: none;
-  cursor: pointer;
-  :hover,active {
-    background-color: initial;
-    background-position: 0 0;
-    color: #FF4742;
-  }
-  :active {
-    opacity: .5;
-  }
 `
 
 const TodoFooter = styled.div`
@@ -148,28 +80,9 @@ const TodoAddButton = styled.button`
 
 function App() {
   const todoList = useTodoList();
+  const { deleteTodoListItem, updateTodoListItem, addTodoListItem } = useTodoListMutation();
   const [todoItem, setTodoItem] = React.useState(todoList);
   const todoRef = React.useRef<HTMLInputElement | null>(null);
-  const queryClient = useQueryClient();
-
-  const deleteTodoListItem = useMutation(api.deleteTodoListItem, {
-    onSuccess: () => {
-      console.log("삭제되었습니다.")
-    }
-  })
-
-  const updateTodoListItem = useMutation(api.updateTodoListItem, {
-    onSuccess: () => {
-      console.log('변경되었습니다.')
-    }
-  })
-
-  const addTodoListItem = useMutation(api.addTodoListItem, {
-    onSuccess: () => {
-      console.log('추가되었습니다.');
-      queryClient.invalidateQueries("todoList");
-    },
-  });
 
   const onHandleDeleteItem = (id: number) => {
     deleteTodoListItem.mutate(id)
@@ -183,7 +96,7 @@ function App() {
 
   const onHandleAddItem = () => {
     const todoText = String(todoRef.current?.value);
-    setTodoItem((current: string[]) => [...current, {id: todoItem.length + 1, title: todoText, complted: false, userId: 1}]);
+    setTodoItem((current: string[]) => [...current, {id: todoItem.length + 1, title: todoText, completed: false, userId: 1}]);
     todoRef.current!.focus();
     addTodoListItem.mutate({id: todoItem.length + 1, title: todoText, completed: false, userId: 1})
   }
@@ -199,43 +112,11 @@ function App() {
         <TodoHeader>
           TodoList
         </TodoHeader>
-        <TodoContent>
-          {
-            todoItem === null ? (
-              <div>
-                loading...
-              </div>
-            ) : (
-              <TransitionGroup>
-              {
-                todoItem.map((items: TodoListItemElements) => (
-                  <CSSTransition
-                    key={items.id}
-                    timeout={500}
-                    classNames="item"
-                    mountOnEnter
-                    unmountOnExit
-                    onExit={() => {
-                      console.log("jeje");
-                    }}
-                    onEnter={() => {
-                      console.log("jeje");
-                    }}>
-                    <TodoListItem>
-                      <ItemCheckBox
-                        type="checkbox"
-                        checked={items.completed} 
-                        onChange={() => onHandleCheckedItem(items)} />
-                      <ItemTitle>{items.title}</ItemTitle>
-                      <ItemDeleteButton onClick={() => onHandleDeleteItem(items.id)}>X</ItemDeleteButton>
-                    </TodoListItem>
-                  </CSSTransition>
-                ))
-              }
-              </TransitionGroup>
-            )
-          }
-        </TodoContent>
+        <TodoContent
+          todoItem={todoItem}
+          onHandleCheckedItem={onHandleCheckedItem}
+          onHandleDeleteItem={onHandleDeleteItem}
+        />
         <TodoFooter>
           <TodoAddInput
             ref={(inputRef) => todoRef.current = inputRef}
